82821c671bbee87dfad8df4615f0d63f71c9cd24
morris <cc12703@gmail.com>
2023-11-16 19:05:56 +0800

    fix configure of project

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
 
   build: {
-    outDir: 'dist_lib',
+    outDir: 'dist',
     lib: {
       entry: resolve(__dirname, './packages/index.ts'),
-      name: 'scrcpy-agent',
-      fileName: 'scrcpy-agent'
+      name: 'scrcpy-vue-agent',
+      fileName: 'scrcpy-vue-agent'
     },
     rollupOptions: {
       external: ['vue'],
